29d88eaa7eb4ff5ae2ccdc4a89a695d98e879a19
nusrat jahan <nusratjahanlima639@gmail.com>
2024-11-20 21:03:15 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var offerEnds = "5 Nov 2024"
+const offerEnds = "5 Nov 2024"
 
 function countdown(){
     const offerDate = new Date(offerEnds)
@@ -7,9 +7,22 @@ function countdown(){
     const totalSeconds = (offerDate - currentDate) / 1000
     
     const days = Math.floor(totalSeconds / (24*60*60))
-    const hours = Math.floor(totalSeconds % (24*60*60)/60*60)
-    console.log(hours);
+    const hours = Math.floor(totalSeconds % (3600*24)/3600)
+    const minutes = Math.floor(totalSeconds % 3600 / 60)
+    const seconds = Math.floor(totalSeconds % 60)
+ 
 
-}   
+    console.log("Days:", days, "Hours:", hours, "Minutes: ",minutes,"Seconds: ", seconds);
+
+    document.getElementById('days').innerText = formatTime(days)
+    document.getElementById('hours').innerText = formatTime(hours)
+    document.getElementById('minutes').innerText = formatTime(minutes)
+    document.getElementById('seconds').innerText = formatTime(seconds)
+}  
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time
+}
 
 countdown()
+setInterval(countdown, 1000)
